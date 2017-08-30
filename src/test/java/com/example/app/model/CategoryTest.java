4589855883b7db3ac6a5cca6f1726dbf866a5ffb
Adrian Hajduk <adrian.hajduk@pl.abb.com>
2017-08-30 15:30:08 +0200

    add first unit test

---
+++ src/test/java/com/example/app/model/CategoryTest.java
@@ -0,0 +1,41 @@
+package com.example.app.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by PLADHAJ on 2017-08-30.
+ */
+public class CategoryTest {
+
+  private Category category;
+
+  @Before
+  public void setUp() {
+    category = new Category();
+  }
+
+  @Test
+  public void getId() throws Exception {
+    Long idValue = 4L;
+
+    category.setId(idValue);
+
+    assertEquals(idValue, category.getId());
+  }
+
+
+  @Test
+  public void getDescription() throws Exception {
+
+  }
+
+
+  @Test
+  public void getRecipes() throws Exception {
+
+  }
+
+}
